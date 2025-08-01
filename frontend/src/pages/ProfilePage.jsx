49ba58b9c5f60630cf6be82e6c6ced1f048ea6aa
Breno Augusto Ferreira <155557831+brenoafsa@users.noreply.github.com>
2025-08-01 15:40:29 -0300

    feat(logout-button)
    adds function to remove token from local storage and redirects user to / page, configures button to execute logOut function

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 import api from "../services/api";
 import { userAuth } from "../services/userAuth";
 import imagem from "/src/assets/img_CInProfile.png"
 
 
 const ProfilePage = () => {
-
+    const navigate = useNavigate();
     const [aluno, setAluno] = useState()
     const token = userAuth.getAccessToken();
+    const [historico, setHistorico] = useState([]);
     const porcentagem = Math.min((30 / 60) * 100, 100) //aqui os valores 3 e 60 devem ser substituidos pelos valores que o aluno tem e o que o curso solicita
 
     useEffect(() => {
@@ -22,6 +23,11 @@ const ProfilePage = () => {
 				.catch((err) => console.error("Erro ao buscar informações do aluno", err))
         }
     }, [token])
+
+    const LogOut = () => {
+        userAuth.removeAccessToken;
+        navigate('/')
+    }
 
     if (!aluno) {
         return (
@@ -51,7 +57,7 @@ const ProfilePage = () => {
 
                         <button type="button" className="bg-[darkred] text-white w-50 rounded-full font-bold ">editar perfil</button>
 
-                        <button className="bg-[white] text-[darkred] border-3 border-indigo- border-t-indigo  w-50  rounded-full font-bold ">sair da conta</button>
+                        <button onClick={LogOut} className="bg-[white] text-[darkred] border-3 border-indigo- border-t-indigo  w-50  rounded-full font-bold ">sair da conta</button>
                     </div>
                     <button type="button" class=" text-[darkred] hover:text-white border border-[darkred]-700 hover:bg-[darkred] focus:ring-4 focus:outline-none focus:ring-[darkred]-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-[darkred]-500 dark:text-[darkred]-500 dark:hover:text-white dark:hover:bg-[darkred]-600 dark:focus:ring-red-900">Histórico de Participação Completo</button>
                     <button type="button" class=" text-[darkred] hover:text-white border border-[darkred]-700 hover:bg-[darkred] focus:ring-4 focus:outline-none focus:ring-[darkred]-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-[darkred]-500 dark:text-[darkred]-500 dark:hover:text-white dark:hover:bg-[darkred]-600 dark:focus:ring-red-900">oportunidades favoritadas</button>
