9cf9c6dbb9f68d47adf51236598e8b7e53ac8742
Breno Augusto Ferreira <155557831+brenoafsa@users.noreply.github.com>
2025-08-05 19:47:02 -0300

    fix(top-bar-buttons-path)
    removes conditional to showcase buttons in topbar and adds path to logo

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,25 +1,21 @@
 import logo from '../assets/logo_linkeCIn.png';
-import { useLocation } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 
 export default function Header() {
   const navigate = useNavigate();
-  const location = useLocation();
-
-  const mostrarOpcoes = location.pathname === '/profile' || location.pathname === '/publish' || location.pathname === '/opportunity/:id' || location.pathname === '/data' || location.pathname === 'favorites' || location.pathname === '/feed';
 
   return (
     <header className="w-full bg-white shadow-md px-8 py-0 sticky top-0 z-30">
       <div className=" w-full flex items-center justify-between">
+        <Link to={"/feed"}>
         <img src={logo} alt="Logo" className="h-18 w-auto" />
+        </Link>
 
-    {mostrarOpcoes && (
       <div className="flex gap-4 font-bold text-[darkred] ">
         <button onClick={() => navigate('/more-info')} className="transition-transform duration-200 hover:scale-105 rounded-md p-2 hover:bg-darkred hover:text-white">Saber Mais</button>
         <button onClick={() => navigate('/profile')} className="transition-transform duration-200 hover:scale-105 rounded-md p-2 hover:bg-darkred hover:text-white">Perfil</button>
       </div>
-    )}
   </div>
 </header>
   );
